6aebb974e24895e3c88aa057819be7de056f511c
thibauttaglialatela <thibauttaglialatela@gmail.com>
2026-09-23 15:44:57 +0200

    feat: :construction: create the footer

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import AppHeader from '@/components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
   <div class="min-h-screen bg-provence-sand text-provence-dark flex flex-col font-sans">
     <AppHeader />
-    
-    <main class="grow w-full max-w-7xl mx-auto px-4 lg:px-20">
+    <main class="grow w-full max-w-7xl mx-auto px-4 lg:px-20 py-8">
       <RouterView />
     </main>
+    <AppFooter />
+
   </div>
 </template>
